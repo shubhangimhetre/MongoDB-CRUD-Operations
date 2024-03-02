30144cc2752f00151fb7e43ed63e58c48292fe25
Shubhangi <shubhangivm7171@gmail.com>
2024-03-02 11:01:30 +0530

    Updated

--- routes/student_routes.js
+++ routes/student_routes.js
@@ -1,14 +1,89 @@
-const router=require('express').Router();
-const students=require('../controllers/studentcontroller');
+const router = require('express').Router();
+const students = require('../controllers/studentcontroller');
 
-router.get('/get_student',students.get_student);
+router.get('/get_student', function (req, res, next) {
 
-router.get('/get_student/:rollno',students.get_student_by_rollno);
+  students.get_student()
+    .then((result) => {
+      return res.status(200).json(result);
+    })
+    .catch((e) => {
+      return res.status(400).json(e);
+    });
 
-router.post('/post_student',students.post_student);
+});
 
-router.put('/update_student/:rollno',students.update_student);
+router.get('/get_student/:rollno', function (req, res, next) {
 
-router.delete('/delete_student/:rollno',students.delete_student);
+  const rollNo = req.params.rollno;
 
-module.exports=router;
+  students.get_student_by_rollno(rollNo)
+    .then((result) => {
+      return res.status(200).json(result);
+    })
+    .catch((e) => {
+      return res.status(400).json(e);
+    });
+
+});
+
+router.post('/post_student', function (req, res, next) {
+
+  let { firstName, lastName, phoneNumber, email, rollNumber } = req.body;
+  students.check_student_Exists(firstName, lastName, phoneNumber, email, rollNumber)
+    .then((result) => {
+      if (result) {
+        return res.status(400).json({ status: "Student Already Exists" });
+      } else {
+        return students.post_student(firstName, lastName, phoneNumber, email, rollNumber)
+      }
+    })
+    .then((result) => {
+      return res.status(200).json(result);
+    })
+    .catch((e) => {
+      return res.status(400).json(e);
+    });
+});
+
+
+router.put('/update_student', function (req, res, next) {
+  let { studentId, firstName, lastName, phoneNumber, email, rollNumber } = req.body;
+  students.get_student_by_rollno(rollNumber)
+    .then((result) => {
+      if (!result) {
+        return res.status(400).json({ status: "Student Not Found" });
+      } else {
+        return students.update_student(studentId, firstName, lastName, phoneNumber, email, rollNumber)
+      }
+    })
+    .then((result) => {
+      return res.status(200).json(result);
+    })
+    .catch((e) => {
+      return res.status(400).json(e);
+    });
+});
+
+
+router.delete('/delete_student/:rollno', function (req, res, next) {
+  const rollNo = req.params.rollno;
+
+  students.get_student_by_rollno(rollNo)
+    .then((result) => {
+      if (!result) {
+        return res.status(400).json({ status: "Student Not Found" });
+      } else {
+        return students.delete_student(rollNo)
+      }
+    })
+    .then((result) => {
+      return res.status(200).json(result);
+    })
+    .catch((e) => {
+      return res.status(400).json(e);
+    });
+});
+
+
+module.exports = router;
